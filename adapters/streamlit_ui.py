e0ebb175494b2b67e42aac89471c16e057d049a0
evaortz <evaortz.art@gmail.com>
2026-09-04 20:34:32 +0200

    Update with palette circles rendering with pack_circles() and st.pyplot()

--- adapters/streamlit_ui.py
+++ adapters/streamlit_ui.py
@@ -1,7 +1,10 @@
 from mural_analyzer.core.pipeline import analyze_image
+from mural_analyzer.core.circle_packing import pack_circles
 import streamlit as st
 from PIL import UnidentifiedImageError
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 
 uploaded_file = st.file_uploader("Upload an image", "image/*")
@@ -11,9 +14,32 @@ merge_threshold = col2.slider("Distance of the colors to blend?", 0, 255, 20, he
 if uploaded_file is not None:
     try:
         palette_info = analyze_image(uploaded_file, n_colors=n_colors, merge_threshold=merge_threshold)
+        
     except UnidentifiedImageError:
         st.error("Error: The file must be an image")
     else:
+
+        #Circulos
+
+        fig, ax = plt.subplots()
+        palette_circles_info = pack_circles(palette_info)
+
+        for circle_item in palette_circles_info:
+            circle = circle_item["circle"]
+            r, g, b = circle_item["rgb"]
+            color = (r/255, g/255, b/255)
+            circle_patch = patches.Circle((circle["x"], circle["y"]), radius=circle["r"], color=color)
+            ax.add_patch(circle_patch)
+
+        ax.set_xlim(-1, 1)
+        ax.set_ylim(-1, 1)
+        ax.set_aspect("equal")
+        ax.axis('off')
+
+        st.pyplot(fig)
+
+
+        #Tarjetas
         _, _, right = st.columns(3)
         if right.toggle("Sort by percentage"):
             palette_info = sorted(palette_info,key=lambda item: item["percentage"], reverse=True)
